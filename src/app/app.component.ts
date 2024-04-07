f6d9cafebbe152fcc6103ce425453a94a401a8d0
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2024-04-07 08:26:54 +0530

    more configurations.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,5 +12,11 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.primengConfig.ripple = true;
+    this.primengConfig.zIndex = {
+      modal: 1100, // dialog, sidebar
+      overlay: 1000, // dropdown, overlaypanel
+      menu: 1000, // overlay menus
+      tooltip: 1100, // tooltip
+    };
   }
 }
